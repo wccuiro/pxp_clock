30c913d0202d0213e1675ebbe949a9cc5eb3b432
Weyner Ccuiro <wey.090300@gmail.com>
2026-03-20 16:22:20 +0100

    small mod

--- plot_scripts/chaos_analysis.py
+++ plot_scripts/chaos_analysis.py
@@ -143,9 +143,24 @@ def process_spectrum(row):
         complex_evals = reals + 1j * imags
         return process_2d_spectrum(g, omega, complex_evals)
 
+
+def print_full(x):
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 2000)
+    pd.set_option('display.float_format', '{:20,.2f}'.format)
+    pd.set_option('display.max_colwidth', None)
+    print(x)
+    pd.reset_option('display.max_rows')
+    pd.reset_option('display.max_columns')
+    pd.reset_option('display.width')
+    pd.reset_option('display.float_format')
+    pd.reset_option('display.max_colwidth')
+
+
 if __name__ == "__main__":
     # Example usage:
-    df = pd.read_csv('../rust/eigenvalues_12.csv', header=None) 
+    df = pd.read_csv('../rust/eigenvalues_10.csv', header=None) 
     results_df = df.apply(process_spectrum, axis=1)
-    print(results_df)
+    print_full(results_df)
     # pass
